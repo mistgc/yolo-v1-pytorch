31dbedd10f4a74b22216a2cf972e8835e91ee41c
mistgc <georgecai0908@outlook.com>
2024-08-15 18:28:36 +0800

    feat: init repo and implement Model

---
+++ src/config.py
@@ -0,0 +1,3 @@
+S = 7       # Divide each image into a SxS grid
+B = 2       # Number of bounding boxes to predict
+C = 20      # Number of classes in the dataset
---
+++ src/model.py
@@ -0,0 +1,73 @@
+import torch
+from torch import nn
+
+from . import config
+
+class YOLOv1(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.depth = config.B * 5 + config.C
+
+        layers = [
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=2),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            nn.Conv2d(192, 128, kernel_size=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Conv2d(256, 256, kernel_size=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ]
+
+        for _ in range(4):
+            layers += [
+                nn.Conv2d(512, 512, kernel_size=1),
+                nn.Conv2d(512, 512, kernel_size=3, padding=1),
+                nn.LeakyReLU(negative_slope=0.1)
+            ]
+
+        layers += [
+            nn.Conv2d(512, 512, kernel_size=1),
+            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ]
+
+        for _ in range(2):
+            layers += [
+                nn.Conv2d(1024, 512, kernel_size=1),
+                nn.Conv2d(512, 1024, kernel_size=3, padding=1),
+                nn.LeakyReLU(negative_slope=0.1)
+            ]
+        layers += [
+            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=2, padding=1),
+            nn.LeakyReLU(negative_slope=0.1),
+        ]
+
+        for _ in range(2):
+            layers += [
+                nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
+                nn.LeakyReLU(negative_slope=0.1)
+            ]
+
+        layers += [
+            nn.Flatten(),
+            nn.Linear(config.S * config.S * 1024, 4096),
+            nn.Dropout(),
+            nn.LeakyReLU(negative_slope=0.1),
+            nn.Linear(4096, config.S * config.S * self.depth)
+        ]
+
+        self.model = nn.Sequential(*layers)
